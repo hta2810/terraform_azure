0ad267bef5dc4c0f5a7d6cc3de3e6e725c471f57
hta2810 <89299418+hta2810@users.noreply.github.com>
2022-03-29 19:03:58 +0700

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,5 +1,4 @@
 locals {
-    resource_name = format("%s-%s-%s", var.assetname, var.enviroment, var.location)
     storage_name = format("storage%s",var.assetname)
     server_name = format("%s%s",var.assetname,var.enviroment)
 }
